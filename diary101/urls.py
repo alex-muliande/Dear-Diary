371ad5fdb31945a82daddc60e6620aa8ce1a6135
alex-muliande <amuliande@gmail.com>
2019-11-01 10:51:45 +0300

    Edit the applications urls

--- diary101/urls.py
+++ diary101/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf.urls import url,include
-from .views import PostListView,PostDetailView,PostCreateView,PostUpdateView,PostDeleteView,ReviewCreateView
 from . import views
 urlpatterns = [
-    path('',  PostListView.as_view(), name='projects-index'),
+    path('',  views.index, name='index'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'^api/profile/$', views.ProfileList.as_view()),
-    path('profile/details/<str:username>/',views.display_profile, name = 'profile-detail'),
 
 
 ]
