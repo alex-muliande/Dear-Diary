b247143b161ef8c3fa00b471d054e9e32926a895
alex-muliande <amuliande@gmail.com>
2019-11-01 10:58:07 +0300

    Add view for the home page

--- diary101/views.py
+++ diary101/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect, get_object_or_404
+from django.http import HttpResponse
+from django.views.generic import CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
+from users.models import Profile
+# from .forms import NeighbourhoodForm,BusinessForm,PostForm
 
 # Create your views here.
+
+
+def index(request):
+
+
+    context= {
+        ""
+    }
+    
+    return render(request,'index.html',{})
